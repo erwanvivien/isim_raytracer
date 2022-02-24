7c9cceee48f3d32f65dfca281edfa7019adbe217
erwan.vivien <erwan.vivien@epita.fr>
2022-02-24 11:15:36 +0100

    fix: using float instead of i64

--- src/vector.rs
+++ src/vector.rs
@@ -1,14 +1,14 @@
 use std::ops::{Add, Mul, Neg, Sub};
 
-#[derive(Debug, Ord, PartialOrd, Eq, PartialEq, Copy, Clone)]
+#[derive(Debug, PartialOrd, PartialEq, Copy, Clone)]
 pub struct Vector {
-    x: i64,
-    y: i64,
-    z: i64,
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
 }
 
 impl Vector {
-    pub fn new(x: i64, y: i64, z: i64) -> Vector {
+    pub fn new(x: f64, y: f64, z: f64) -> Vector {
         Vector { x, y, z }
     }
 }
@@ -24,9 +24,9 @@ impl Add<Vector> for Vector {
     }
 }
 
-impl Add<i64> for Vector {
+impl Add<f64> for Vector {
     type Output = Vector;
-    fn add(self, rhs: i64) -> Self::Output {
+    fn add(self, rhs: f64) -> Self::Output {
         Vector {
             x: self.x + rhs,
             z: self.z + rhs,
@@ -35,9 +35,9 @@ impl Add<i64> for Vector {
     }
 }
 
-impl Sub<i64> for Vector {
+impl Sub<f64> for Vector {
     type Output = Vector;
-    fn sub(self, rhs: i64) -> Self::Output {
+    fn sub(self, rhs: f64) -> Self::Output {
         self + (-rhs)
     }
 }
@@ -61,15 +61,15 @@ impl Neg for Vector {
 }
 
 impl Mul for Vector {
-    type Output = i64;
+    type Output = f64;
     fn mul(self, rhs: Self) -> Self::Output {
         self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
     }
 }
 
-impl Mul<i64> for Vector {
+impl Mul<f64> for Vector {
     type Output = Vector;
-    fn mul(self, rhs: i64) -> Self::Output {
+    fn mul(self, rhs: f64) -> Self::Output {
         Vector {
             x: self.x * rhs,
             y: self.y * rhs,
@@ -83,55 +83,55 @@ mod tests {
 
     #[test]
     fn add_vec() {
-        let v1 = Vector::new(1, 2, -4);
-        let v2 = Vector::new(5, 6, -7);
+        let v1 = Vector::new(1f64, 2f64, -4f64);
+        let v2 = Vector::new(5f64, 6f64, -7f64);
 
         let v = v1 + v2;
 
-        assert_eq!(v, Vector::new(6, 8, -11))
+        assert_eq!(v, Vector::new(6f64, 8f64, -11f64))
     }
 
     #[test]
     fn add_num() {
-        let v1 = Vector::new(1, 2, -4);
+        let v1 = Vector::new(1f64, 2f64, -4f64);
 
-        let v = v1 + 5;
+        let v = v1 + 5f64;
 
-        assert_eq!(v, Vector::new(6, 7, 1))
+        assert_eq!(v, Vector::new(6f64, 7f64, 1f64))
     }
 
     #[test]
     fn sub_vec() {
-        let v1 = Vector::new(1, 2, -4);
-        let v2 = Vector::new(5, 6, -7);
+        let v1 = Vector::new(1f64, 2f64, -4f64);
+        let v2 = Vector::new(5f64, 6f64, -7f64);
 
         let v = v1 - v2;
 
-        assert_eq!(v, Vector::new(-4, -4, 3))
+        assert_eq!(v, Vector::new(-4f64, -4f64, 3f64))
     }
 
     #[test]
     fn sub_num() {
-        let v1 = Vector::new(1, 2, -4);
+        let v1 = Vector::new(1f64, 2f64, -4f64);
 
-        let v = v1 - 3;
+        let v = v1 - 3f64;
 
-        assert_eq!(v, Vector::new(-2, -1, -7))
+        assert_eq!(v, Vector::new(-2f64, -1f64, -7f64))
     }
 
     #[test]
     fn norm_vec() {
-        let v1 = Vector::new(2, 4, 7);
-        let v2 = Vector::new(-2, 1, 0);
+        let v1 = Vector::new(2f64, 4f64, 7f64);
+        let v2 = Vector::new(-2f64, 1f64, 0f64);
 
-        assert_eq!(v1 * v2, 0)
+        assert_eq!(v1 * v2, 0f64)
     }
 
     #[test]
     fn mul_num() {
-        let mut v1 = Vector::new(2, 4, 0);
-        v1 = v1 * 5;
+        let mut v1 = Vector::new(2f64, 4f64, 0f64);
+        v1 = v1 * 5f64;
 
-        assert_eq!(v1, Vector::new(10, 20, 0))
+        assert_eq!(v1, Vector::new(10f64, 20f64, 0f64))
     }
 }
