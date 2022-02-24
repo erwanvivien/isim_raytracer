3e748773b8af272a5ed77b76fddcdb0bc5a299de
erwan.vivien <erwan.vivien@epita.fr>
2022-02-25 00:21:38 +0100

    fix: remove dead code & added link

--- src/object/sphere.rs
+++ src/object/sphere.rs
@@ -12,22 +12,14 @@ pub struct Sphere {
 
 impl Intersect for Sphere {
     fn is_intersect(&self, p: Point, v: Vector) -> bool {
+        // taken from
+        // http://ambrsoft.com/TrigoCalc/Sphere/SpherLineIntersection_.htm
         // a = (x2 - x1)2 + (y2 - y1)2 + (z2 - z1)2
         // b = - 2[(x2 - x1)(xc - x1) + (y2 - y1)(yc - y1) + (z2 - z1)(zc - z1)]
         // c = (xc - x1)2 + (yc - y1)2 + (zc - z1)2 - r2
         let (x1, x2, xc) = (p.x, v.x + p.x, self.p.x);
         let (y1, y2, yc) = (p.y, v.y + p.y, self.p.y);
         let (z1, z2, zc) = (p.z, v.z + p.z, self.p.z);
-
-        // let cp = (p - self.p).to_vec();
-        //
-        // let a = v.normalize();
-        // let a = a * a;
-        //
-        // let b = (v.x * cp);
-        // let b = b * -2f64;
-        //
-        // let c = (cp.x.powf(2f64) + cp.y.powf(2f64) + cp.z.powf(2f64)) - self.r.powf(2f64);
 
         let a = (x2 - x1).powf(2f64) + (y2 - y1).powf(2f64) + (z2 - z1).powf(2f64);
         let b = (x2 - x1) * (xc - x1) + (y2 - y1) * (yc - y1) + (z2 - z1) * (zc - z1);
